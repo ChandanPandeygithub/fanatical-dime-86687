a8cd6128b5c76f5926648ab592e8f427e8391d65
chandan pandey <77069375+ChandanPandeygithub@users.noreply.github.com>
2023-01-19 21:40:25 +0530

    day-3

--- clone/src/App.js
+++ clone/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import ImageSlider from "./Components/ImageSlider";
 import Navbar from "./Components/Navbar";
+import SortingBar from "./Components/SortingBar";
 
 function App() {
   return (
     <div className="App">
       <div style={{ position: "sticky", top: "1px" }}>
         <Navbar />
+        <SortingBar/>
       </div>
     
      </div>
--- clone/src/Components/SortingBar.js
+++ clone/src/Components/SortingBar.js
@@ -1,8 +1,59 @@
-import React from 'react'
+import { MdSort } from "react-icons/md";
+import {
+  Box,
+  Button,
+  Divider,
+  Flex,
+  HStack,
+  Image,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
+  Spacer,
+} from "@chakra-ui/react";
+import React from "react";
 
-function SortingBar() {
+export default function SortingBar() {
   return (
-  )
+    <Flex
+      p={2}
+      paddingLeft={10}
+      gap="2"
+      minWidth={"max-content"}
+      justifyContent={"center"}
+      alignItems={"center"}
+      alignContent={"center"}
+      bg={" 2px solid black"}
+    >
+      <Box>
+        <HStack>
+          <Image
+            src="https://media6.ppl-media.com/static/purplle/img/purplle-logo-1.svg"
+            boxSize={{ base: "md", lg: "fit-content" }}
+          />
+          <Image
+            src="https://media6.ppl-media.com/mediafiles/ecomm/promo/1537798844_try-logo-3x.png"
+            boxSize={{ base: "md", lg: "10" }}
+          />
+        </HStack>
+      </Box>
+      <Spacer />
+      <Box>
+      <Menu >
+      <MenuButton as ={Button} leftIcon={<MdSort/>}>sort</MenuButton>
+           <MenuList gap={10} width={"full"} border={"2px solid blue"} display={"flex"} >
+            <MenuItem display={"flex"} flexDirection={"column"}>
+            <MenuButton as ={Button}> action</MenuButton>
+            <MenuList display="flex" flexDirection={"column"}>
+            <menuItem>12</menuItem>
+            </MenuList>
+            </MenuItem>
+          
+           </MenuList>
+           </Menu>
+      </Box>
+      <Spacer />
+    </Flex>
+  );
 }
-
-export default SortingBar
